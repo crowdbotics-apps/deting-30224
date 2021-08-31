a7f74f40d73fbff8134b2ed3f72647d06b23c9d3
Crowdbotics <team@crowdbotics.com>
2021-08-31 18:00:29 +0000

    Update models

--- backend/deting_30224/wsgi.py
+++ backend/deting_30224/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deting_30224.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deting_30224.settings")
 
 application = get_wsgi_application()
