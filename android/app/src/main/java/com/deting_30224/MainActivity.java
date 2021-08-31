f207034a6da7682607bddf95fd32a700d6785e84
Crowdbotics <team@crowdbotics.com>
2021-08-31 17:58:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/deting_30224/MainActivity.java
@@ -0,0 +1,15 @@
+package com.deting_30224;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "deting_30224";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "deting_30224Identifier",
+  url: "https://deting_30224Identifier.botics.co",
+  api: "https://deting_30224Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
